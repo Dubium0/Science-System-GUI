bc77ddb1390afa4d2aef04777c343fdbe13847b4
Yunus Emre ASLAN <40874281+Dubium0@users.noreply.github.com>
2022-01-25 19:19:30 +0300

    Update locoGraph2.py

--- locoGraph2.py
+++ locoGraph2.py
@@ -9,7 +9,7 @@ motor_dataL = {"velocity":[0],"voltage":[0],"temperature":[0],"current":[0]}
 motor_dataR= {"velocity":[0],"voltage":[0],"temperature":[0],"current":[0]}
 
 def plotting(data):
-    data_list = data.data.split(",")
+    data_list = data.data.split(";")
     
     motor_dataL["velocity"].append(float(data_list[0]))
     motor_dataL["voltage"].append(float(data_list[1]))
@@ -19,29 +19,54 @@ def plotting(data):
     motor_dataR["voltage"].append(float(data_list[5]))
     motor_dataR["temperature"].append(float(data_list[6]))
     motor_dataR["current"].append(float(data_list[7]))
-    
-    plt.subplot(4,1,1)
-    plt.plot(motor_dataL["velocity"],color = "blue")
-    plt.plot(motor_dataR["velocity"],color = "red")
-    plt.ylabel("velocity")
-    
-    plt.subplot(4,1,2)
-    plt.plot(motor_dataL["voltage"],color = "blue")
-    plt.plot(motor_dataR["voltage"],color = "red")
-    plt.ylabel("voltage")
-    
-    plt.subplot(4,1,3)
-    plt.plot(motor_dataL["temperature"],color = "blue")
-    plt.plot(motor_dataR["temperature"],color = "red")
-    plt.ylabel("temperature")
 
-    plt.subplot(4,1,4)
-    plt.plot(motor_dataL["current"],color = "blue")
-    plt.plot(motor_dataR["current"],color = "red")
-    plt.ylabel("current")
-    
-    plt.draw()
-    plt.pause(0.0001)
+
+    if len(motor_dataL["velocity"]) <100:
+        plt.subplot(4,1,1)
+        plt.plot(motor_dataL["velocity"],color = "blue")
+        plt.plot(motor_dataR["velocity"],color = "red")
+        plt.ylabel("velocity")
+        
+        plt.subplot(4,1,2)
+        plt.plot(motor_dataL["voltage"],color = "blue")
+        plt.plot(motor_dataR["voltage"],color = "red")
+        plt.ylabel("voltage")
+        
+        plt.subplot(4,1,3)
+        plt.plot(motor_dataL["temperature"],color = "blue")
+        plt.plot(motor_dataR["temperature"],color = "red")
+        plt.ylabel("temperature")
+
+        plt.subplot(4,1,4)
+        plt.plot(motor_dataL["current"],color = "blue")
+        plt.plot(motor_dataR["current"],color = "red")
+        plt.ylabel("current")
+        
+        plt.draw()
+        plt.pause(0.0001)
+    else:
+        plt.subplot(4,1,1)
+        plt.plot(motor_dataL["velocity"][-100:],color = "blue")
+        plt.plot(motor_dataR["velocity"][-100:],color = "red")
+        plt.ylabel("velocity")
+        
+        plt.subplot(4,1,2)
+        plt.plot(motor_dataL["voltage"][-100:],color = "blue")
+        plt.plot(motor_dataR["voltage"][-100:],color = "red")
+        plt.ylabel("voltage")
+        
+        plt.subplot(4,1,3)
+        plt.plot(motor_dataL["temperature"][-100:],color = "blue")
+        plt.plot(motor_dataR["temperature"][-100:],color = "red")
+        plt.ylabel("temperature")
+
+        plt.subplot(4,1,4)
+        plt.plot(motor_dataL["current"][-100:],color = "blue")
+        plt.plot(motor_dataR["current"][-100:],color = "red")
+        plt.ylabel("current")
+        
+        plt.draw()
+        plt.pause(0.0001)
     
 
 
